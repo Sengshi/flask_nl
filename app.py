c3fb9c8decf17beeb39f5966ba73ae444ceac17c
Denis Kel <asket2013@ya.ru>
2023-06-16 12:16:01 +1000

    Change to aiohttp

--- app.py
+++ app.py
@@ -1,79 +1,187 @@
-from flask import Flask, jsonify, request, abort, session
-from models import Post, session
+import json
 
-app = Flask("app")
+# from flask import Flask, jsonify, request, abort, session
+from models import Post, Session, engine, Base
+from aiohttp import web
+from sqlalchemy.exc import IntegrityError
 
+# Flask
+# app = Flask("app")
 
-class HttpError(Exception):
-    def __init__(self, status_code: int, message: str | dict | list):
-        self.status_code = status_code
-        self.message = message
-
-
-@app.errorhandler(HttpError)
-def error_handler(er: HttpError):
-    response = jsonify({"status": "error", "message": er.message})
-    response.status_code = er.status_code
-    return response
+# AIOHTTP
+app = web.Application()
 
 
-@app.route("/posts", methods=["GET", "POST"])
-def create():
-    if request.method == "GET":
-        posts = session.query(Post).all()
-        return jsonify([post.to_json() for post in posts])
-    if request.method == "POST":
-        if not request.json:
-            abort(400)
-        post = Post(
-            title=request.json.get('title'),
-            author=request.json.get('author'),
-            description=request.json.get('description')
+# Flask
+# class HttpError(Exception):
+#     def __init__(self, status_code: int, message: str | dict | list):
+#         self.status_code = status_code
+#         self.message = message
+#
+#
+# @app.errorhandler(HttpError)
+# def error_handler(er: HttpError):
+#     response = jsonify({"status": "error", "message": er.message})
+#     response.status_code = er.status_code
+#     return response
+#
+#
+# @app.route("/posts", methods=["GET", "POST"])
+# def create():
+#     if request.method == "GET":
+#         posts = session.query(Post).all()
+#         return jsonify([post.to_json() for post in posts])
+#     if request.method == "POST":
+#         if not request.json:
+#             abort(400)
+#         post = Post(
+#             title=request.json.get('title'),
+#             author=request.json.get('author'),
+#             description=request.json.get('description')
+#         )
+#         session.add(post)
+#         session.commit()
+#         return jsonify(post.to_json()), 201
+#
+#
+# @app.route("/posts/<int:id>", methods=["GET", "PATCH", "DELETE"])
+# def detail_post(id):
+#     if request.method == "GET":
+#         post = session.query(Post).get(id)
+#         if post is None:
+#             abort(404)
+#         return jsonify(
+#             {
+#                 "id": post.id,
+#                 "title": post.title,
+#                 "description": post.description,
+#                 "author": post.author,
+#                 "create_date": post.create_date.isoformat(),
+#             }
+#         )
+#     if request.method == "PATCH":
+#         post = session.query(Post).get(id)
+#         print(request.json.get("author"))
+#
+#         post.title = request.json.get("title", post.title)
+#         post.author = request.json.get("author", post.author)
+#         post.description = request.json.get("description", post.description)
+#         session.commit()
+#         return jsonify(
+#             {
+#                 "id": post.id,
+#                 "title": post.title,
+#                 "description": post.description,
+#                 "author": post.author,
+#                 "create_date": post.create_date.isoformat(),
+#             }
+#         )
+#     if request.method == "DELETE":
+#         post = session.query(Post).get(id)
+#         if post:
+#             session.delete(post)
+#             session.commit()
+#         return jsonify(), 204
+
+# AIOHTTP
+@web.middleware
+async def session_middleware(request: web.Request, handler):
+    async with Session() as session:
+        request["session"] = session
+        response = await handler(request)
+        return response
+
+
+async def get_post(post_id: int, session: Session):
+    post = await session.get(Post, post_id)
+    if post is None:
+        raise web.HTTPNotFound(
+            text=json.dumps({"error": "post not found"}),
+            content_type="application/json",
         )
-        session.add(post)
-        session.commit()
-        return jsonify(post.to_json()), 201
+    return post
 
 
-@app.route("/posts/<int:id>", methods=["GET", "PATCH", "DELETE"])
-def detail_post(id):
-    if request.method == "GET":
-        post = session.query(Post).get(id)
-        if post is None:
-            abort(404)
-        return jsonify(
+class PostView(web.View):
+    @property
+    def session(self):
+        return self.request["session"]
+
+    @property
+    def post_id(self):
+        return int(self.request.match_info["post_id"])
+
+    async def get(self):
+        post = await get_post(self.post_id, self.session)
+
+        return web.json_response(
             {
-                "id": post.id,
-                "title": post.title,
-                "description": post.description,
-                "author": post.author,
-                "create_date": post.create_date.isoformat(),
+                'id': post.id,
+                'title': post.title,
+                'description': post.description,
+                'author': post.author,
+                'create_date': int(post.create_date.timestamp())
             }
         )
-    if request.method == "PATCH":
-        post = session.query(Post).get(id)
-        print(request.json.get("author"))
 
-        post.title = request.json.get("title", post.title)
-        post.author = request.json.get("author", post.author)
-        post.description = request.json.get("description", post.description)
-        session.commit()
-        return jsonify(
-            {
-                "id": post.id,
-                "title": post.title,
-                "description": post.description,
-                "author": post.author,
-                "create_date": post.create_date.isoformat(),
-            }
-        )
-    if request.method == "DELETE":
-        post = session.query(Post).get(id)
-        if post:
-            session.delete(post)
-            session.commit()
-        return jsonify(), 204
+    async def post(self):
+        json_data = await self.request.json()
+        post = Post(**json_data)
+        try:
+            self.session.add(post)
+            await self.session.commit()
+        except IntegrityError as er:
+            raise web.HTTPConflict(
+                text=json.dumps({"error": "post already exists"}),
+                content_type="application/json",
+            )
+        return web.json_response({"id": post.id})
 
+    async def patch(self):
+        json_data = await self.request.json()
+        post = await get_post(self.post_id, self.session)
+        for field, value in json_data.items():
+            setattr(post, field, value)
+        try:
+            self.session.add(post)
+            await self.session.commit()
+        except IntegrityError as er:
+            raise web.HTTPConflict(
+                text=json.dumps({"error": "post already exists"}),
+                content_type="application/json",
+            )
+        return web.json_response({"id": post.id})
+
+    async def delete(self):
+        post = await get_post(self.post_id, self.session)
+        await self.session.delete(post)
+        await self.session.commit()
+        return web.json_response({"id": post.id})
+
+
+async def orm_context(app: web.Application):
+    print("START")
+    async with engine.begin() as con:
+        await con.run_sync(Base.metadata.create_all)
+    yield
+    print("SHUT DOWN")
+    await engine.dispose()
+
+
+app.cleanup_ctx.append(orm_context)
+app.middlewares.append(session_middleware)
+app.add_routes(
+    [
+        web.post("/posts/", PostView),
+        web.get("/posts/{post_id:\d+}", PostView),
+        web.patch("/posts/{post_id:\d+}", PostView),
+        web.delete("/posts/{post_id:\d+}", PostView),
+    ]
+)
 
 if __name__ == "__main__":
-    app.run()
+    # Flask
+    # app.run()
+
+    # AIOHTTP
+    web.run_app(app)
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from atexit import register
 
 from sqlalchemy import Column, DateTime, Integer, String, create_engine, func
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,15 +10,20 @@ DB_PASSWORD = 'django'
 DB_ADDRESS = 'localhost'
 DB_PORT = 5432
 DB_NAME = 'nl_flask'
-DB_DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}"
+# DB_DSN = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}"
+DB_DSN = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_ADDRESS}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DB_DSN)
+# Flask
+# engine = create_engine(DB_DSN)
+# register(engine.dispose)
+# Session = sessionmaker(bind=engine)
+# session = Session()
+# Base = declarative_base()
 
-register(engine.dispose)
-
-Session = sessionmaker(bind=engine)
-session = Session()
+# AIOHTTP
+engine = create_async_engine(DB_DSN)
 Base = declarative_base()
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Post(Base):
@@ -28,14 +34,14 @@ class Post(Base):
     author = Column(String(50), nullable=False)
     create_date = Column(DateTime, server_default=func.now())
 
-    def to_json(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'description': self.description,
-            'author': self.author,
-            'create_date': self.create_date
-        }
+    # def to_json(self):
+    #     return {
+    #         'id': self.id,
+    #         'title': self.title,
+    #         'description': self.description,
+    #         'author': self.author,
+    #         'create_date': self.create_date
+    #     }
 
 
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
